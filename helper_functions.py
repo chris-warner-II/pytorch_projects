180a5686615ca0a2583dcfe6d9a7325d194d66dd
CWarner <world7one@gmail.com>
2023-10-25 12:10:48 -0700

    Implemented Linear Regression on linear, multi-dimensional dataset.

--- helper_functions.py
+++ helper_functions.py
@@ -3,25 +3,47 @@
 import matplotlib.pyplot as plt
 
 
-
-def plot_predictions(X_train=X_train,
-                     y_train=y_train,
-                     X_test=X_test,
-                     y_test=y_test,
+def plot_predictions(train_data,
+                     train_labels,
+                     test_data,
+                     test_labels,
                      predictions=None):
     """
-    Function to plot data & predictions
+    Function to plot training and test data vs. labels for linear regression dataset
+    as well as test data vs. model predictions.
 
-    :param X_train:
-    :param y_train:
-    :param X_test:
-    :param y_test:
+    :param train_data:
+    :param train_labels:
+    :param test_data:
+    :param test_labels:
     :param predictions:
-    :return:
+    :return: None
     """
     plt.figure(figsize=(10, 7))
-    plt.scatter(X_train, y_train, c='b', s=6, label="Train")
-    plt.scatter(X_test, y_test, c='y', s=6, label="Test")
+    plt.scatter(train_data, train_labels, c='b', s=15, label="Train")
+    plt.scatter(test_data, test_labels, c='g', s=15, label="Test")
+    plt.xlabel('Data')
+    plt.ylabel('Label')
     if predictions is not None:
-        plt.scatter(X_test, predictions, c='r', s=6, label="Predictions")
+        plt.scatter(test_data, predictions, c='r', s=15, marker='x', label="Predictions")
     plt.legend(prop={"size": 14})
+
+
+def plot_loss(epoch, loss, test_loss=None,y_scale='linear'):
+    """
+
+    :param epoch:
+    :param loss:
+    :param test_loss:
+    :param y_scale
+    :return: None
+    """
+    plt.figure(figsize=(10,7))
+    plt.plot(epoch, loss, c='b', label='Train')
+    if test_loss is not None:
+        plt.plot(epoch, test_loss, c='r', label='Test')
+    plt.yscale(y_scale)
+    plt.legend(prop={"size": 14})
+
+
+
--- models.py
+++ models.py
@@ -3,10 +3,56 @@
 import torch
 from torch import nn
 
+
+def train_step_regression(data, labels, model, loss_fn, optimizer):
+    """
+    Define the training step to be used within training loop.
+    Predict responses to train_data.
+    Calculate loss between predictions & train_labels.
+    Perform backprop with loss.
+    Perform gradient descent.
+    :return: loss
+    """
+
+    model.train()
+
+    preds = model(data)
+    loss = loss_fn(preds, labels)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return(loss)
+
+
+def test_step_regression(data, labels, model, loss_fn):
+    """
+    Define test step to be used to compute test_loss.
+    Predict responses to test_data with model params fixed.
+    Calculate loss
+    :return: test_loss
+    """
+
+    model.eval()
+
+    with torch.inference_mode():
+        preds = model(data)
+
+    loss = loss_fn(preds, labels)
+
+    return(loss)
+
+
+
 class LinearRegressionModelV0(nn.Module):
-    def __init__(self):
+    """
+    A Linear Regression model with 'in_dim' weight and 1 bias parameter to fit a line in
+    'in_dim' dimensions.
+    """
+    def __init__(self,in_dim):
         super().__init__()
-        self.linear_layer1 = nn.Linear(in_features=1,
+        self.linear_layer1 = nn.Linear(in_features=in_dim,
                                        out_features=1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
